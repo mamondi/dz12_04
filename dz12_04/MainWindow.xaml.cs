70a0a9019aeef481089d43513c347f38ff1910d2
DimamonDESKTOP <dimamon02@gmail.com>
2024-04-15 15:15:29 +0300

    done

--- dz12_04/MainWindow.xaml.cs
+++ dz12_04/MainWindow.xaml.cs
@@ -1,28 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace dz12_04
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void saveArray_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int[] array = {
+                    int.Parse(frstarrayInput.Text),
+                    int.Parse(scndarrayInput.Text),
+                    int.Parse(thrdarrayInput.Text),
+                    int.Parse(frtharrayInput.Text),
+                    int.Parse(fiftharrayInput.Text)
+                };
+
+                string filePath = @"D:\Projects\c#\dz12_04\dz12_04\array.txt";
+                File.WriteAllLines(filePath, Array.ConvertAll(array, x => x.ToString()));
+
+                MessageBox.Show("Масив збережено в файлі array.txt", "Успіх");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Будь ласка, введіть числа в усі поля", "Помилка");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Сталася помилка: " + ex.Message, "Помилка");
+            }
         }
     }
 }
